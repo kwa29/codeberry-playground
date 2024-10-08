e1499282825f3630b04fdb1fa9a2c27bb1393c0a
Didier Caroff <djkwa29@gmail.com>
2024-10-08 11:23:07 +0200

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+}
 
 export default nextConfig;
